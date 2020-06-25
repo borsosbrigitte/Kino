3cf9b741548aeef30535dd88df786b94dafb33e9
unknown <borsosbrigitte@gmail.com>
2020-06-25 21:49:06 +0300

    Added a whole lot of features

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
 from .models import Film, UserList, FilmList
@@ -5,6 +6,9 @@ from .models import Film, UserList, FilmList
 urlpatterns = [
     path('', views.index, name='index'),
     path('film/<int:pk>', views.FilmDetailView.as_view(model=Film), name='film_detail'),
-    path('watchlists/', views.UserListsView.as_view(model=UserList), name='watchlists'),
-    path('watchlists/<int:pk>', views.WatchListView.as_view(model=FilmList), name='watchlist'),
+    path('watchlists/', login_required(views.UserListsView.as_view(model=UserList)), name='watchlists'),
+    path('watchlists/<int:pk>', views.watch_list_view, name='watchlist'),
+    path('watchlists/<int:pk>/delete/', views.delete_watchlist_view, name='delete_watchlist'),
+    path('watchlists/<int:pk>/edit/', views.edit_watchlist_name_view, name='edit_watchlist_name'),
+    path('watchlists/<int:wpk>/delete_film/<int:fpk>', views.delete_watchlist_film, name='delete_watchlist_film'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
 
@@ -22,9 +24,8 @@ class FilmDetailView(generic.DetailView):
         # Call the base implementation first to get the context
         context = super(FilmDetailView, self).get_context_data(**kwargs)
         # Create any data and add it to the context
-        user_id = User.objects.filter(username__exact="bri").first()
         film_ratings = FilmRating.objects\
-            .filter(user_id__exact=user_id)\
+            .filter(user_id__exact=self.request.user.pk)\
             .filter(film_id__exact=self.object.pk)
 
         no_of_ratings = film_ratings.count()
@@ -40,14 +41,54 @@ class UserListsView(generic.ListView):
     template_name = "catalog/watchlists.html"
 
     def get_queryset(self):
-        user_id = User.objects.filter(username__exact="bri").first()
-        return UserList.objects.filter(user_id__exact=user_id)
+        return UserList.objects.filter(user_id__exact=self.request.user.pk)
+
+    def post(self, request, **kwargs):
+        watchlist_name = request.POST.get('name_field')
+        watchlist = UserList(user_id=self.request.user, list_name=watchlist_name)
+        watchlist.save()
+        return HttpResponseRedirect('/watchlists/')
+
+@login_required
+def watch_list_view(request, pk):
+    film_list_items = FilmList.objects.filter(list_id__exact=pk)
+    film_pks = []
+    for film in film_list_items:
+        film_pks.append(film.film_id.pk)
+
+    films = Film.objects.filter(pk__in=film_pks)
+
+    watchlist = UserList.objects.filter(pk__exact=pk).first()
+    context = {
+        'watchlist': watchlist,
+        'films': films
+    }
+
+    return render(request, 'catalog/watchlist.html', context=context)
 
 
-class WatchListView(generic.ListView):
-    model = FilmList
-    template_name = "catalog/watchlist.html"
+@login_required()
+def delete_watchlist_view(request, pk):
+    UserList.objects.filter(pk=pk).delete()
+    return HttpResponseRedirect('/watchlists/')
 
-    def get_queryset(self):
-        user_id = User.objects.filter(username__exact="bri").first()
-        print(user_id)
+
+@login_required()
+def edit_watchlist_name_view(request, pk):
+    if request.method == 'GET':
+        watchlist = UserList.objects.filter(pk=pk).first()
+        context = {
+            'watchlist': watchlist
+        }
+
+        return render(request, 'catalog/edit_watchlist_name.html', context=context)
+    elif request.method == 'POST':
+        UserList.objects.filter(pk=pk).update(list_name=request.POST.get('name_field'))
+        return HttpResponseRedirect('/watchlists/')
+
+
+@login_required()
+def delete_watchlist_film(request, wpk, fpk):
+    FilmList.objects.filter(list_id=wpk).filter(film_id=fpk).delete()
+    return HttpResponseRedirect('../../../watchlists/' + str(wpk))
+
